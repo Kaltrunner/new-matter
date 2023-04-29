3c2de3f5f27624ba65bcfb7151c46ecde90d48be
Erik Russell <erikrussells@gmail.com>
2023-04-29 16:26:35 -0400

    pretty much done with this project, just waiting to see if there are any more design tips from joyce

--- src/componenets/home/Home.jsx
+++ src/componenets/home/Home.jsx
@@ -2,47 +2,6 @@ import "./home.css";
 import { useState, useEffect } from "react";
 
 function Home() {
-  // function Clock() {
-  //   const [time, setTime] = useState(new Date());
-
-  //   useEffect(() => {
-  //     const intervalId = setInterval(() => {
-  //       setTime(new Date());
-  //     }, 1000);
-  //     return () => clearInterval(intervalId);
-  //   }, []);
-
-  //   let days = [
-  //     "Sunday",
-  //     "Monday",
-  //     "Tuesday",
-  //     "Wednesday",
-  //     "Thursday",
-  //     "Friday",
-  //     "Saturday",
-  //   ];
-
-  //   const formattedTime = () => {
-  //     let day = days[time.getDay()];
-  //     let h = time.getHours();
-  //     let m = time.getMinutes();
-  //     let s = time.getSeconds();
-  //     m = checkTime(m);
-  //     s = checkTime(s);
-
-  //     return day + " " + h + ":" + m + ":" + s;
-  //   };
-
-  //   const checkTime = (i) => {
-  //     if (i < 10) {
-  //       i = "0" + i;
-  //     }
-  //     return i;
-  //   };
-
-  //   return <div id="landing-txt">{formattedTime()}</div>;
-  // }
-
   return (
     <>
       <div className="header-body">
@@ -54,15 +13,18 @@ function Home() {
         <div className="row home-data-div" id="data-row-div-home">
           <div className="col" id="data-col"></div>
           <div className="col" id="data-col">
-          Utilizing advanced technologies to analyze extraterrestrial samples, we can unravel the mysteries of our universe. 
+            Utilizing advanced technologies to analyze extraterrestrial samples,
+            we can unravel the mysteries of our universe.
           </div>
           <div className="col" id="data-col">
-          Furthermore, focusing on Betelgeuse allows us to deepen our understanding of stellar evolution and the mechanisms behind its recent dimming.
+            Furthermore, focusing on Betelgeuse allows us to deepen our
+            understanding of stellar evolution and the mechanisms behind its
+            recent dimming.
           </div>
           <div className="col" id="data-col">
             Your visit is greatly appreciated, thank you for the exploration.
-            <br/>
-            <br/>
+            <br />
+            <br />
             Designed and developed by{" "}
             <a
               id="portfolio-link"
@@ -70,32 +32,12 @@ function Home() {
               rel="noreferrer"
               href="https://www.erikmrussell.com"
             >
-              Erik
+              Erik Russell
             </a>{" "}
-            & Joyce Russell — ©2023
+             ©2023
           </div>
         </div>
       </div>
-
-      {/* <div className="container text-center" id="data-col-row-top-bottom">
-        <div className="row home-data-div" id="data-row-div-home">
-          <div className="col" id="data-col"></div>
-          <div className="col" id="data-col"></div>
-          <div className="col" id="data-col"></div>
-          <div className="col" id="data-col">
-            Designed and developed by{" "}
-            <a
-              id="portfolio-link"
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.erikmrussell.com"
-            >
-              Erik
-            </a>{" "}
-            & Joyce Russell — ©2023
-          </div>
-        </div>
-      </div> */}
 
       <div className="marquee-div">
         <p className="marquee">
